85ac1ce1fbfde182233ae7b695717f99f93bd6f7
Vitosnatios <vitorxxxnoob@gmail.com>
2022-08-23 14:14:59 -0300

    now checks jwt token whenever the user open the 'confirm purchase' window, some refactoring and some new server res handlers on login, buying and editing profile pages, etc

--- src/LoginContext.jsx
+++ src/LoginContext.jsx
@@ -65,7 +65,7 @@ export function useCheckJwt(){
     return useContext(CheckJwt)
 }
 //endereço sem barra no final pf
-export const serverUrl = 'http://localhost:5000';
+export const serverUrl = 'https://vitos-e-commerce.herokuapp.com/';
 
 export function IsLoggedInProvider({ children }){
 
